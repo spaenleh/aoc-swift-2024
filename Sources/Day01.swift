fe973c0679bf77b2b0a36d8ee364ecddbafbb5f2
spaenleh <spaenleh@gmail.com>
2024-12-01 10:37:13 +0100

    improve code for day1

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -8,22 +8,25 @@ struct Day01: AdventDay {
     data.split(separator: "\n").map { $0.split(separator: " ") }
   }
 
+  var left: [Int] {
+    lines.map { Int($0[0]) ?? 0 }.sorted()
+  }
+
+  var right: [Int] {
+    lines.map { Int($0[1]) ?? 0 }.sorted()
+  }
+
   // add here any computed values useful for the challenge
 
   func part1() -> Int {
-    let listRight = lines.map { Int($0[0]) ?? 0 }.sorted()
-    let listLeft = lines.map { Int($0[1]) ?? 0 }.sorted()
     var sum = 0
-    for (one, two) in zip(listLeft, listRight) {
+    for (one, two) in zip(left, right) {
       sum += abs(one - two)
     }
     return sum
   }
 
   func part2() -> Int {
-    let left = lines.map { Int($0[0]) ?? 0 }.sorted()
-    let right = lines.map { Int($0[1]) ?? 0 }.sorted()
-
     var similarity = 0
     for elem in left {
       let occurrences = right.filter { $0 == elem }.count
